84ef50c32e2620c7533bf337f6c0edb839b4d3f6
rodrigobosarreyes <rodrigobosarreyes@outlook.es>
2021-05-31 22:04:52 +0200

    Project section + cards

--- src/core/components/project-card/ProjectCard.tsx
+++ src/core/components/project-card/ProjectCard.tsx
@@ -3,50 +3,53 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { Component } from 'react';
 import './ProjectCard.scss';
 
+type ProjectCardProps = {
+  idArrElement: string,
+  idLeftElement: string,
+  imgUrl: string
+}
 
-export class ProjectCard extends Component {
+export class ProjectCard extends Component<ProjectCardProps> {
   arrowIcon = <FontAwesomeIcon icon={faArrowRight} className="rb__icon"/>;
   cancelIcon = <FontAwesomeIcon icon={faTimes} className="cancel__icon"/>;
 
   onClickCancel = () => {
-    const arr = document.querySelector('.arr__container');
-    const leftContainer = document.querySelector('.left__container');
-    arr?.classList.remove('arr__container');
-    if (leftContainer?.classList.contains('active__arr')) {
-      leftContainer.classList.remove('active__arr');
+    const arr = document.getElementById(this.props.idArrElement);
+    const leftContainer = document.getElementById(this.props.idLeftElement);
+    arr?.classList.remove('active_arr');
+    if (leftContainer?.classList.contains('active')) {
+      leftContainer.classList.remove('active');
       leftContainer.classList.add('off');
     }
   }
 
   onClickArrow = () => {
-    console.log('onclick arrow');
-    
-    const arr = document.querySelector('.arr__container');
-    const leftContainer = document.querySelector('.left__container');
-    arr?.classList.add('arr__container');
+    const arr = document.getElementById(this.props.idArrElement);
+    const leftContainer = document.getElementById(this.props.idLeftElement);
+    arr?.classList.add('arr_container');
     if (leftContainer?.classList.contains('off')) {
       leftContainer.classList.remove('off');
-      leftContainer.classList.add('active__arr');
+      leftContainer.classList.add('active');
     }
   }
   
   render() {
     return (
-      <div className="project_card__container">
-        <div className="background__container">
-          <div className="arr__container rb_center" onClick={this.onClickArrow}>
+      <div className="project_card__container center">
+        <div className="project_card__content center" style={{backgroundImage: `url(${this.props.imgUrl})`}}>
+          <div className="arr_container center" id={this.props.idArrElement} onClick={this.onClickArrow}>
             {this.arrowIcon}
           </div>
-        </div>
-        {/* <div className="left__container off">
-          <p>Hello world</p>
-          <div className="center cancel" onClick={this.onClickCancel}>
-            {this.cancelIcon}
+          <div className="left_container off" id={this.props.idLeftElement}>
+            <p>Skills</p>
+            <div className="cancel center" onClick={this.onClickCancel}>
+              {this.cancelIcon}
+            </div>
           </div>
-        </div> */}
+        </div>
       </div>
     )
   }
 }
 
-export default ProjectCard
+export default ProjectCard;
--- src/pages/home/components/projects/Projects.tsx
+++ src/pages/home/components/projects/Projects.tsx
@@ -3,15 +3,28 @@ import { ProjectCard } from '../../../../core/components/project-card/ProjectCar
 import './Projects.scss';
 
 export class Projects extends Component {
+  projects = [
+    {idArrElement: 'arr-1', idLeftElement: 'left-1', imgUrl: 'https://dummyimage.com/601x400/000/fff'},
+    {idArrElement: 'arr-2', idLeftElement: 'left-2', imgUrl: 'https://dummyimage.com/602x400/000/fff'},
+    {idArrElement: 'arr-3', idLeftElement: 'left-3', imgUrl: 'https://dummyimage.com/603x400/000/fff'},
+    {idArrElement: 'arr-4', idLeftElement: 'left-4', imgUrl: 'https://dummyimage.com/604x400/000/fff'},
+    {idArrElement: 'arr-5', idLeftElement: 'left-5', imgUrl: 'https://dummyimage.com/605x400/000/fff'},
+    {idArrElement: 'arr-6', idLeftElement: 'left-6', imgUrl: 'https://dummyimage.com/606x400/000/fff'},
+  ]
+  
   render() {
     return (
       <div className="bg-gray">
         <section id="projects" className="container">
           <h2>PROJECTS</h2>
           <div className="projects__content row no-gutters">
-            <div className="col-3">
-              <ProjectCard />
-            </div>
+            {this.projects.map( p => {
+              return (
+                <div className="col-3 my-3">
+                  <ProjectCard idArrElement={p.idArrElement} idLeftElement={p.idLeftElement} imgUrl={p.imgUrl} />
+                </div>
+              )
+            })}
             {/* <div className="col-3">
               <ProjectCard />
             </div>
